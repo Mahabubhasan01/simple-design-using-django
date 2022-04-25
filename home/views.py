2c928ddb86a60c363bb7871e72cb0f39cf470b9f
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-04-25 14:55:01 +0600

    models and admin panel updated

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
+
 from django.shortcuts import render ,HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -8,4 +13,14 @@ def services(request):
 def about(request):
     return render(request,'about.html')
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, 'Profile details updated.')
     return render(request,'contact.html')
+
+
